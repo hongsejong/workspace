2c8414daaa34b75069c28d86117365c67d3cce00
홍세종 <hsj3779@gmail.com>
2025-03-19 11:24:48 +0900

    스프링부트 게시판 / 조건문

--- 8_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ 8_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -9,8 +9,8 @@ public class MainController {
 	
 	@RequestMapping("/")
 	public String mainForward(Model model) {
-
-		model.addAttribute("message","스프링부트 테스트중");
+		model.addAttribute("name","홍길동");
+		//model.addAttribute("message","스프링부트 테스트중");
 		
 		// Spring MVC : /webapp/WEB-INF/views/common/main.jsp
 		
